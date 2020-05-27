c3a103c8b515a873429e63d4c5041bb28dd64ecd
Bjarki Baldursson Harksen <bjarkibharksen@Bjarkis-MBP.lan>
2020-05-27 18:56:18 +0000

    slide finished

--- skabretti.py
+++ skabretti.py
@@ -38,36 +38,43 @@ class MovingBlock(Block):
         "start_point": LEFT,
         "end_point": RIGHT,
         "align_block_edge_to_path": True,
-        "distance_traveled": 0,
+        "displacement": 0,
         "velocity": 0,
         "acceleration": 0,
     }
 
     def __init__(self, **kwargs):
         Block.__init__(self, **kwargs)
-        path = self.get_def_path()
+        path = Line(self.start_point, self.end_point)
+        # self.displacement = self.initial_displacement
         ang = path.get_angle()
-        l = path.get_length()
+        self.path_length = path.get_length()
 
         if self.align_block_edge_to_path:
             path.shift(np.array([self.height/2 * math.sin(-ang), self.height/2 * math.cos(-ang), 0]))
-            path.set_length(path.get_length()-self.width)
-            l = path.get_length()
+            path.set_length(self.path_length-self.width)
+            self.path_length = path.get_length()
 
-        self.rotate(ang)
-        self.add_updater(lambda b: b.move_to(straight_path(
-            path.get_start(), path.get_end(), self.distance_traveled / path.get_length())))
-        
         self.true_path = path
+        self.rotate(ang)
+        self.move_to(path.get_start())
 
-    def get_def_path(self):
-        return Line(self.start_point, self.end_point)
+        def update_block(self, dt):
+            self.velocity += self.acceleration * dt
+            self.displacement += self.velocity * dt
+            self.move_to(straight_path(self.true_path.get_start(), self.true_path.get_end(), self.displacement / self.path_length))
+            if self.displacement >= self.path_length:
+                self.velocity = 0
+                self.acceleration = 0
+                self.move_to(self.true_path.get_end())
 
-    def get_true_path(self):
-        return self.true_path
+        self.add_updater(update_block)
 
-    def move(self, d):
-        self.distance_traveled = d
+    def start_move(self, a, v=0):
+        self.acceleration = a
+        self.velocity = v
+    
+
 
 
 class InclinedPlane(Polygon):
@@ -131,18 +138,24 @@ class BlockSlidingDownInclinedPlane(Scene):
         # Values (in SI-units)
         "m": 1,
         "theta": 30,  # degrees
-        "mu_k": 0.5,
-        "mu_s": 0.5,
+        "mu_k": 0.2,
+        "mu_s": 0.2,
         "g": 9.82,
+        "v0": 0,
 
         "show_legend": True,
+        "combine_mu": True,
         "show_ind_force_vectors": False,
         "show_total_force_vector": False,
         "total_force_color": COLOR_MAP["RED_C"],
         "show_force_names": False,
+        "time_before_slide": 2,
     }
 
     def construct(self):
+        if self.combine_mu:
+            self.mu_s = self.mu_k
+        
         incl_plane = InclinedPlane(
             angle=self.theta, kinetic_coefficient_of_friction=self.mu_k, static_coefficient_of_friction=self.mu_s)
         incl_plane.to_corner(DL)
@@ -151,21 +164,35 @@ class BlockSlidingDownInclinedPlane(Scene):
 
         block = MovingBlock(mass=self.m, start_point=incl_plane.get_vertices()[
                             1], end_point=incl_plane.get_vertices()[2])
+        
+        a = self.g * (math.sin(ang) - self.mu_k * math.cos(ang))
+        total_time = (-self.v0 + math.sqrt(self.v0**2 +2*a*block.path_length)) / a
+        if self.mu_s >= math.tan(ang):
+            a = 0
+            total_time = 0
 
         if self.show_legend:
-            self.add_legend()
+            self.add_legend(self.combine_mu)
 
         self.add(incl_plane, block)
-        self.wait(1)
+        self.wait(self.time_before_slide)
+        block.start_move(a, self.v0)
 
-        dist_tracker = ValueTracker(0)
-        block.add_updater(lambda b: b.move(dist_tracker.get_value()))
-        self.play(dist_tracker.set_value,
-                  block.get_true_path().get_length(), run_time=3)
-        self.wait(1)
+        self.wait(total_time + 1)
 
-    def add_legend(self, position=UR, scale=0.7,
-                   show_m=True, show_theta=True, show_mu_k=True, show_mu_s=True, show_g=True):
+        # if self.mu_s < math.tan(ang):
+        #     # block.start_move(self.g * (math.sin(ang) - self.mu_k * math.cos(ang)))
+        #     block.start_move(self.g * (math.sin(ang) - self.mu_k * math.cos(ang)), self.v0)
+        #     self.wait()
+
+        # dist_tracker = ValueTracker(0)
+        # block.add_updater(lambda b: b.move(dist_tracker.get_value()))
+        # self.play(dist_tracker.set_value,
+        #           block.get_true_path().get_length(), run_time=3)
+        # self.wait(1)
+
+    def add_legend(self, combine_mu, position=UR, scale=0.7,
+                   show_m=True, show_theta=True, show_mu_k=True, show_mu_s=True, show_g=False):
 
         m_leg = TexMobject("m", "=", "\SI{%s}{kg}" % (self.m))
         m_leg[0].align_to(m_leg[1], RIGHT).next_to(m_leg[1], LEFT)
@@ -176,6 +203,11 @@ class BlockSlidingDownInclinedPlane(Scene):
         theta_leg[1].align_to(m_leg[1], LEFT)
         theta_leg[2].align_to(m_leg[2], LEFT)
 
+        mu_leg = TexMobject("\\mu", "=", "\\num{%s}" % (self.mu_k))
+        mu_leg[0].align_to(m_leg[0], RIGHT)
+        mu_leg[1].align_to(m_leg[1], LEFT)
+        mu_leg[2].align_to(m_leg[2], LEFT)
+        
         mu_k_leg = TexMobject("\\mu_k", "=", "\\num{%s}" % (self.mu_k))
         mu_k_leg[0].align_to(m_leg[0], RIGHT)
         mu_k_leg[1].align_to(m_leg[1], LEFT)
@@ -198,8 +230,11 @@ class BlockSlidingDownInclinedPlane(Scene):
         if show_theta:
             legend.add(theta_leg)
         if show_mu_k:
-            legend.add(mu_k_leg)
-        if show_mu_s:
+            if combine_mu:
+                legend.add(mu_leg)
+            else:
+                legend.add(mu_k_leg)
+        if show_mu_s and not combine_mu:
             legend.add(mu_s_leg)
         if show_g:
             legend.add(g_leg)
